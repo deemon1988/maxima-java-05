92ee697cc3ba49412a07237c89e938f7ec5876d4
deemon1988 <dmn72835@gmail.com>
2023-01-02 00:22:35 +0300

    5.2 Servlet API

--- src/main/java/org/example/CatServlet.java
+++ src/main/java/org/example/CatServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-
+ 
 @WebServlet("/cat")
 public class CatServlet extends HttpServlet {
 
